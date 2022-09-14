e77086b81cd539e098e4edf7c6dd41fefb99a43c
yusuf-inci <103791586+yusuf-inci@users.noreply.github.com>
2022-09-14 22:35:28 +0300

    Update chores.sh

--- chores.sh
+++ chores.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 #sudo du -hsx /var/* | sort -rh | head -1
-PS3="Enter a chore number."
+PS3="Enter a chore number:"
 while true; do
-    select choice in ErrorLogFiles AddUser LatestLogin TopTenHomes Exit; do
+    select choice in ErrorLogFiles AddUser LatestLogin TopTenHomes TopTenVar Exit; do
         case $choice in
+        ErrorLogFiles)
+            sudo grep -i error /var/log/*
+            break
+            ;;
+        AddUser)
+            ./add_user.sh
+            break
+            ;;
+        TopTenHomes)
+            ./top10.sh home
+            break
+            ;;
+        LatestLogin)
+            last -10
+            break
+            ;;
+        TopTenVar)
+            ./top10.sh var
+            break
+            ;;
         Exit)
             exit 0
             break
